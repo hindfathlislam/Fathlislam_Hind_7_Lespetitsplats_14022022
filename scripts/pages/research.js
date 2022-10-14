191d76f7cc3d52e406a7f39344a9c26fb39b618d
hindfathlislam <hindfathlislam@gmail.com>
2022-10-14 16:13:46 +0200

    functionnal version

--- scripts/pages/research.js
+++ scripts/pages/research.js
@@ -1,41 +1,44 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 function research(recipes) {
+
     const input = document.querySelector(".input");
     const searchResult = document.querySelector(".recipes");
-
-    // const item = faire une liste là comme le json structure 
-    // const input = 'un item au pif pour verifier que en combien de temps ca le trouve"
-
+  
     // Filtrer les recettes
     input.addEventListener("input", (e) => {
         const searchedString = e.target.value.toLowerCase();
-
+  
         // à partir de 3 caractères
         if (searchedString.length > 2) {
             searchResult.innerHTML = "";
-// for loop to see if the content of the input match description/ingredient  or name, if it does display tje recepe card dom with the value matching [i]
-            for (let i in recipes) {
-                const recipeTitle = recipes[i].name.toLowerCase().includes(searchedString);
-                const recipeDescription = recipes[i].description.toLowerCase().includes(searchedString);
-
-                if (recipeTitle || recipeDescription) {
-                    const recipeModel = recipesFactory(recipes[i]);
-                    const recipeCardDOM = recipeModel.getRecipesCardDOM();
-                    searchResult.appendChild(recipeCardDOM);
+            console.log(recipes);
+            const filteredRecipe = recipes.filter(recipe => {
+                const nameMatch = `${recipe.name}`.toLowerCase().includes(searchedString);
+                const descriptionMatch = `${recipe.description}`.toLowerCase().includes(searchedString);
+                const numberOfIngredient = recipe.ingredients.filter(item => `${item.ingredient}`.toLowerCase().includes(searchedString)).length;
+  
+                if (numberOfIngredient > 0 || nameMatch || descriptionMatch) {
+                    return recipe;
                 }
-            }
-        } 
-
-        // Remet toutes les recette si la barre de recherche est vide
-        if (searchedString.length === 0) {
-            searchResult.innerHTML = "";
-            for (let i in recipes) {
-                const recipeModel = recipesFactory(recipes[i]);
+            });
+  
+            // Affichage des recettes
+            filteredRecipe.forEach((recipe) => {
+                const recipeModel = recipesFactory(recipe);
                 const recipeCardDOM = recipeModel.getRecipesCardDOM();
                 searchResult.appendChild(recipeCardDOM);
-            }
+            });
         }
-
+  
+        // Remet toutes les recette si la barre de recherche est vide
+        if (searchedString.length === 0) {
+  
+            recipes.forEach((recipe) => {
+                const recipeModel = recipesFactory(recipe);
+                const recipeCardDOM = recipeModel.getRecipesCardDOM();
+                searchResult.appendChild(recipeCardDOM);
+  
+            });
+        }
+  
     });
-}
+  }
